f79c62bafdc44d89c2219763d7daa1644dda529c
dmitriyyarnov <78679511+dmitriyyarnov@users.noreply.github.com>
2025-10-26 15:23:53 +0300

    грфаик Plotly

--- main.py
+++ main.py
@@ -51,7 +51,6 @@ def index(request: Request, month: str = Query(None)):
     filtered = [cr for cr in credits if cr[3].startswith(month)]
     total_month = sum(cr[2] for cr in filtered)
 
-    # monthly_totals больше не нужен, убираем
     return templates.TemplateResponse("index.html", {
         "request": request,
         "credits": filtered,
@@ -116,7 +115,6 @@ def index(request: Request, month: str = Query(None)):
     filtered = [cr for cr in credits if cr[3].startswith(month)]
     total_month = sum(cr[2] for cr in filtered)
 
-    # Для графика Plotly
     monthly_totals = {}
     for _, _, amount, due_date, _ in credits:
         ym = due_date[:7]
